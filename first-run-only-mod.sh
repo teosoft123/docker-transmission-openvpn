2d01c636c2251cb7348e2f94c274eb19bd776638
Oleg Tsvinev <otsvinev@vmware.com>
2018-03-14 19:57:32 -0700

    Removing excess info

--- first-run-only-mod.sh
+++ first-run-only-mod.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+WORKDIR=/home/oleg/projects/dto/
 docker run --cap-add=NET_ADMIN --device=/dev/net/tun -d \
--v /home/oleg/projects/dto/data/:/data \
+-v ${WORKDIR}/data/:/data \
 -v /etc/localtime:/etc/localtime:ro \
---env-file docker-env-file.conf \
+--env-file ${WORKDIR}/docker-env-file.conf \
 --log-driver json-file \
 --log-opt max-size=10m \
 -p 8181:9091 \
 --dns 8.8.8.8 \
 --dns 8.8.4.4 \
 --name=xm-ovpn \
+--restart unless-stopped \
 teosoft/dto
